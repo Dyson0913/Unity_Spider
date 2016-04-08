22b0bc9330dca944fc288d7e4da6b7b770854f1e
Dyson0913 <hhg4092@gmail.com>
2016-04-08 19:31:41 +0800

    dk in half

---
+++ Lobby/Assets/UIScript/UI_Buttonlist.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class UI_Buttonlist : MonoBehaviour {
+
+	public List<Button> _list;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Lobby/Assets/UIScript/UI_Enable.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class UI_Enable : MonoBehaviour {
+
+	public List<GameObject> _ItemList;
+
+	public void item_init()
+	{
+		for (int i=0; i< _ItemList.Count; i++) {
+			_ItemList[i].gameObject.SetActive (false);
+		}
+	}
+
+	public void item_set(string state)
+	{
+		if (state == "into_game") {
+			for( int i=0;i< _ItemList.Count;i++)
+			{
+				_ItemList[i].gameObject.SetActive (true);			
+			}
+		}
+		if (state == "back_lobby") {
+			for( int i=0;i< _ItemList.Count;i++)
+			{
+				_ItemList[i].gameObject.SetActive (false);			
+			}
+		}
+	}
+
+}
